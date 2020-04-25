c3fbfafd7e47a1759a93b3538b507f02a062bbc6
“[pradnesh <[pradneshsanderan@gmail.com]>
2020-04-25 15:10:24 +0100

    Updates

---
+++ precache-manifest.03d6ece1e5a2e2283d8d90d6aa661f1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0e11dfe7f366bb6b45de3cbcd8edc4",
+    "url": "/pradneshsanderan/index.html"
+  },
+  {
+    "revision": "e9afb3c8257ccbeb9a15",
+    "url": "/pradneshsanderan/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "b84743b87726430bb37c",
+    "url": "/pradneshsanderan/static/css/main.c9b18bff.chunk.css"
+  },
+  {
+    "revision": "e9afb3c8257ccbeb9a15",
+    "url": "/pradneshsanderan/static/js/2.14010ef2.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/pradneshsanderan/static/js/2.14010ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84743b87726430bb37c",
+    "url": "/pradneshsanderan/static/js/main.32642bec.chunk.js"
+  },
+  {
+    "revision": "17398f817b2cfad1bca2",
+    "url": "/pradneshsanderan/static/js/runtime-main.dd779ad4.js"
+  },
+  {
+    "revision": "f900883ef5f4043d0af4a31f8310ea54",
+    "url": "/pradneshsanderan/static/media/Background.f900883e.png"
+  }
+]);
